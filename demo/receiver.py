25b2964f0979dc5e354fb38d0dcf2a0290dd8434
Marcel Truong-Chun <76450818+marceltc@users.noreply.github.com>
2026-09-23 19:02:58 -0700

    Add response body

--- demo/receiver.py
+++ demo/receiver.py
@@ -14,8 +14,12 @@ class Receiver(BaseHTTPRequestHandler):
             self.send_error(404)
             return
         if self.path == "/path/to/some/repo/with/dependency":
+            body = b"OK\n"
             self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.send_header("Content-Length", str(len(body)))
             self.end_headers()
+            self.wfile.write(body)
             return
 
         length = int(self.headers.get("Content-Length", "0"))
